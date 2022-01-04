8700bc63601cd2bd11b53867bce6d9b1bc536b39
billy <biilaaal212@gmail.com>
2022-01-04 16:24:24 +0100

    quelques modifications à faire

--- index.php
+++ index.php
@@ -8,30 +8,30 @@
   }
 ?>
 <!DOCTYPE html>
-<html>
-  <head>
-  <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="sucess">
-    <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
-    <p>C'est votre tableau de bord.</p>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+  <div class="wrapper">
+    <header>To Do List</header>
+    <div class="inputField">
+      <input type="text" placeholder="Ajoutez vos taches">
+      <button><i class="fas fa-plus"></i></button>
+    </div>
+    <ul class="todoList">
 
+    </ul>
+    <div class="footer">
+      <span>Vous avez <span class="pendingTasks"></span> tache(s)</span>
+      <button>Supprimez tout</button>
+    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
+<script src="script.js"></script>
     <a href="logout.php">Déconnexion</a>
     </div>
   </body>
